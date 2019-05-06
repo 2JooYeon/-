2b1a98bd1c67a3c2cf2af05b6f94199595f479b3
LeeJooYeon <lewndus7749@naver.com>
2019-05-06 22:11:46 +0900

    project 입력

--- 이주연.개인과제/이주연.개인과제/이주연.개인과제.cpp
+++ 이주연.개인과제/이주연.개인과제/이주연.개인과제.cpp
@@ -1,21 +1,96 @@
-﻿// 이주연.개인과제.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
-//
+﻿#define _CRT_SECURE_NO_WARNINGS
+#define MIN(a, b) (((a) < (b)) ? (a) : (b))
+#define MAX(a, b) (((a) > (b)) ? (a) : (b))
+#include <stdio.h>
+#include <CoalaMOD.h>
+#pragma comment(lib, "CoalaMOD.lib")
 
-#include "pch.h"
-#include <iostream>
 
-int main()
-{
-    std::cout << "Hello World!\n"; 
+int main() {
+
+	//두점 입력받기
+	int sx, sy, sz, ex, ey, ez;
+	printf("두 점의 좌표를 입력하세요:");
+	scanf("%d %d %d %d %d %d", &sx, &sy, &sz, &ex, &ey, &ez);
+
+	int x1 = MIN(sx, ex);
+	int y1 = MIN(sy, ey);
+	int z1 = MIN(sz, ez);
+	int x2 = MAX(sx, ex);
+	int y2 = MAX(sy, ey);
+	int z2 = MAX(sz, ez);
+	int x, y, z;
+	int a = (x1 + x2) / 2;
+	int b = (z1 + z2) / 2;
+
+	//건물 생성에 필요한 블록 만들기
+	BricksID bricks1 = createBricks(BRICKS_STONE);
+	PlanksID wood = createPlanks(PLANKS_DARK_OAK);
+	WoolID leaf = createWool(COLOR_LIME);
+	BlockID dirt = createBlock(BLOCK_DIRT);
+
+
+	//화단 만들기
+	for (x = x1; x <= x2; x++) {
+		for (z = z1; z <= z2; z++) {
+			locateBlock(bricks1, x, y1, z);
+		}
+	}
+	//화단에 흙 채우기
+	for (x = x1 + 3; x < (x2 - 2); x++) {
+		for (z = z1 + 3; z < (z2 - 2); z++) {
+			locateBlock(dirt, x, y1, z);
+		}
+	}
+
+
+	//새싹 만들기
+	if (y2 - y1 <= 10) {
+		//새싹 줄기&잎 만들기
+		for (y = y1 + 1; y <= (y2 - 2); y++) {
+			locateBlock(leaf, (x1 + x2) / 2, y, (z1 + z2) / 2);
+		}
+		locateBlock(leaf, a + 1, y2 - 1, b);
+		locateBlock(leaf, a + 2, y2, b);
+		locateBlock(leaf, a - 1, y2 - 1, b);
+		locateBlock(leaf, a - 2, y2, b);
+	}
+
+
+	//나무 만들기
+	else {
+		//나무기둥 만들기
+		for (x = x1 + 8; x < (x2 - 7); x++) {
+			for (z = z1 + 8; z < (z2 - 7); z++) {
+				for (y = y1; y <= y2; y++) {
+					locateBlock(wood, x, y, z);
+				}
+			}
+		}
+
+		//나뭇잎 만들기
+		for (x = x1 + 4; x < (x2 - 3); x++) {
+			for (z = z1 + 4; z < (z2 - 3); z++) {
+				locateBlock(leaf, x, y2, z);
+			}
+		}
+		for (x = x1 + 2; x < (x2 - 1); x++) {
+			for (z = z1 + 2; z < z2 - 1; z++) {
+				locateBlock(leaf, x, y2 - 2, z);
+				locateBlock(leaf, x, y2 - 4, z);
+			}
+		}
+		for (x = x1 + 3; x < (x2 - 2); x++) {
+			for (z = z1 + 3; z < (z2 - 2); z++) {
+				locateBlock(leaf, x, y2 - 1, z);
+			}
+		}
+		for (x = x1 + 1; x < x2; x++) {
+			for (z = z1 + 1; z < z2; z++) {
+				locateBlock(leaf, x, y2 - 3, z);
+			}
+		}
+	}
+
+
 }
-
-// 프로그램 실행: <Ctrl+F5> 또는 [디버그] > [디버깅하지 않고 시작] 메뉴
-// 프로그램 디버그: <F5> 키 또는 [디버그] > [디버깅 시작] 메뉴
-
-// 시작을 위한 팁: 
-//   1. [솔루션 탐색기] 창을 사용하여 파일을 추가/관리합니다.
-//   2. [팀 탐색기] 창을 사용하여 소스 제어에 연결합니다.
-//   3. [출력] 창을 사용하여 빌드 출력 및 기타 메시지를 확인합니다.
-//   4. [오류 목록] 창을 사용하여 오류를 봅니다.
-//   5. [프로젝트] > [새 항목 추가]로 이동하여 새 코드 파일을 만들거나, [프로젝트] > [기존 항목 추가]로 이동하여 기존 코드 파일을 프로젝트에 추가합니다.
-//   6. 나중에 이 프로젝트를 다시 열려면 [파일] > [열기] > [프로젝트]로 이동하고 .sln 파일을 선택합니다.
